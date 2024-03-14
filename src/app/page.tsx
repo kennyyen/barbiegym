3c1c240685938cc38b5ce7307587b4201a0eb138
Kenny Yen <k27058083@yahoo.com.tw>
2024-03-14 21:33:38 +0900

    style update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-
 import { HeaderSection } from '~/components/home-page/header-section';
 import { PageWrapper } from '~/components/home-page/page-wrapper';
 import { UsSection } from '~/components/home-page/us-section';
 import { CombineSection } from '~/components/home-page/combined-section';
-
-
 
 export default async function Home() {
   console.log('Rendering home page');
--- src/components/home-page/combined-section/index.tsx
+++ src/components/home-page/combined-section/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import StatementSection from "./statement-section";
-import TestimonialSection from "./testimonial-section";
+import StatementSection from './statement-section';
+import TestimonialSection from './testimonial-section';
 
 export const CombineSection = () => {
-    const backgroundStyle = {
-        backgroundImage: `url('images/gym-bg.jpg')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'repeat',
-        backgroundPosition: 'center center',
-        backgroundAttachment: 'scroll'
-    };
+  const backgroundStyle = {
+    backgroundImage: 'url("images/gym-bg.jpg")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'repeat',
+    backgroundPosition: 'center center',
+    backgroundAttachment: 'scroll',
+  };
 
-    return (
-        <div style={backgroundStyle} className="text-white min-h-full">
-            <StatementSection />
-            <TestimonialSection />
-        </div>
-    );
+  return (
+    <div className="text-white min-h-full" style={backgroundStyle}>
+      <StatementSection />
+      <TestimonialSection />
+    </div>
+  );
 };
